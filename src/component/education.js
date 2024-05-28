64256bafbd44794ba62006aee756328135f17f7e
Noel Kim <97138799+N0ELcs@users.noreply.github.com>
2024-05-27 23:34:30 -0700

    Add files via upload

---
+++ src/component/education.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const education = () => {
+    return (
+        <section id="education">
+            <h2>Education/Skills</h2>
+            <p>University of California, Riverside</p>
+        </section>
+    );
+}
+
+export default education;
+
---
+++ src/component/extra.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const extra = () => {
+    return (
+        <section id="extra">
+            <h2>Extracurricular</h2>
+            <p>hackathon</p>
+        </section>
+    );
+}
+
+export default extra;
+
